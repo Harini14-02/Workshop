9fbf576102fb08931a25a52d8af3118e34093b98
Harini14-02 <63644518+Harini14-02@users.noreply.github.com>
2023-11-25 11:43:12 +0530

    Full Stack

---
+++ script.js
@@ -0,0 +1,15 @@
+var a=10;
+var b=10;
+var c;
+const d = 10;
+c=a+b+d;
+var a=13;
+console.log(c);
+
+function add(){
+    var a=document.getElementById(number1).value;
+    var b=document.getElementById(number2).value;
+
+    var c =parseInt(a)+parseInt(b)
+    document.getElementById("answer").value=c;
+}
